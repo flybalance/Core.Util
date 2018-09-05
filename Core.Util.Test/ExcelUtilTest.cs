acdeff440f06247cdc5afe71930ee1ab00eaf9b2
admin <mas@mysteel.com>
2018-09-05 11:39:17 +0800

    添加单元测试

---
+++ Core.Util.Test/ExcelUtilTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Xunit;
+
+namespace Core.Util.Test
+{
+    public class ExcelUtilTest
+    {
+        [Fact]
+        public void ImportExcelTest()
+        {
+            string filePath = @"D:/NewDeskTop/阿里巴巴Java开发手册终极版v1.3.0 - 副本.xls";
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            var result = ExcelUtil.ImportExcel(fileStream, 2);
+            Assert.True(null != result && result.Count > 0);
+
+        }
+    }
+}
