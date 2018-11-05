b4677142273c787d78c2d748ea95d18095aade26
admin <mas@mysteel.com>
2018-11-05 11:21:08 +0800

    1、添加StringUtil、FileUtil类

---
+++ Core.Util/FileUtil.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Core.Util
+{
+    /// <summary>
+    /// 文件操作帮助类
+    /// </summary>
+    public static class FileUtil
+    {
+        private static List<FileInfo> lst = new List<FileInfo>();
+
+        /// <summary>
+        /// 获得目录下所有文件或指定文件类型文件(包含所有子文件夹)
+        /// </summary>
+        /// <param name="path">文件夹路径</param>
+        /// <param name="extName">扩展名可以多个 例如 *.mp3 *.wma *.rm</param>
+        /// <returns><![CDATA[List<FileInfo>]]></returns>
+        public static List<FileInfo> GetFile(string path, string extName)
+        {
+            try
+            {
+                // 文件夹列表
+                string[] dir = Directory.GetDirectories(path);
+                DirectoryInfo fdir = new DirectoryInfo(path);
+                // 获取指定后缀的文件列表
+                FileInfo[] file = fdir.GetFiles(extName);
+                // 当前目录文件或文件夹不为空
+                if (file.Length > 0)
+                {
+                    // 显示当前目录所有文件
+                    lst.AddRange(file);
+                }
+
+                if (dir.Length > 0)
+                {
+                    foreach (string d in dir)
+                    {
+                        // 递归子目录
+                        GetFile(d, extName);
+                    }
+                }
+
+                return lst;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Core.Util/StringUtil.cs
@@ -0,0 +1,27 @@
+﻿namespace Core.Util
+{
+    public static class StringUtil
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static bool IsOutOfAllow(string str, int length)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+
+            if (str.Length <= length)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
